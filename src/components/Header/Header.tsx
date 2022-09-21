2296fdef1bf8ef67709164e27230b677b733b94a
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2022-09-21 16:01:04 +0800

    responsive font sizes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,7 @@
-
-
 export function Header() {
-    return(
-        <div className="p-4 w-full bg-black text-white text-center">
+    return (
+        <div className="p-4 w-full bg-black text-white text-2xl md:text-3xl font-extrabold text-center">
             <h2>Tech Talks Singapore</h2>
         </div>
-    )
+    );
 }
--- src/components/TitleCard/TitleCard.tsx
+++ src/components/TitleCard/TitleCard.tsx
@@ -4,9 +4,9 @@ export function TitleCard() {
             <span className=" text-gray-600">
                 <time>Tuesday, September 27, 2022</time>
             </span>
-            <h1 className="font-extrabold text-4xl">
+            <h1 className="font-extrabold text-2xl md:text-3xl">
                 Component Driven (UI) Development using React, Storybook
             </h1>
         </div>
-    )
+    );
 }
