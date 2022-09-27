68c86f1a9436a8b60d77d53f8e054dc9789279f1
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2022-09-27 14:35:21 +0800

    update membercard stories

--- src/components/MemberCard/MemberCard.stories.tsx
+++ src/components/MemberCard/MemberCard.stories.tsx
@@ -1,11 +1,11 @@
 import { cdd } from '../../data/events';
-import { MemberCard } from './MemberCard';
+import { MemberCard as MemberCardComponent } from './MemberCard';
 
 export default {
     title: 'Components/MemberCard',
-    component: MemberCard,
+    component: MemberCardComponent,
 };
 
-export const Compact = () => <MemberCard member={cdd.attendees[0]} />;
-
-export const Regular = () => <MemberCard member={cdd.attendees[1]} />;
+export const MemberCard = () => (
+    <MemberCardComponent member={cdd.attendees[0]} />
+);
